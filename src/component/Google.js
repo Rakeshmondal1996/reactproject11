51f066909fc27f9cf9ea0cf2a92cbc26400f3e42
tejas6083 <tejas6083@gmail.com>
2020-07-17 02:05:17 +0530

    Login Redirect Sorted

--- src/component/Google.js
+++ src/component/Google.js
@@ -1,66 +1,40 @@
-import React, { Component } from 'react';
-import GoogleLogin from 'react-google-login';
-import { GoogleLogout } from 'react-google-login';
-import { Link } from 'react-router-dom';
-import { Redirect } from 'react-router-dom'
-import { withRouter } from 'react-router-dom'
+import React, { Component } from "react";
+import GoogleLogin from "react-google-login";
+import { GoogleLogout } from "react-google-login";
+import history from "./History";
+import { Button } from "react-bootstrap";
 
-class Google extends Component { 
-  constuctor() {
-    this.routeChange = this.routeChange.bind(this);
+class Google extends Component {
+  // constuctor() {
+  //   this.routeChange = this.routeChange.bind(this);
+  // }
+
+  responseGoogle(response) {
+    history.push("/Home");
   }
-  
-    responseGoogle = (response) => 
-    {
-       
-      let path = `/Home`;
-      this.props.history.push(path);
-        alert("Hi Bunty you have successfully Login");
-        
-     console.log(response);
-     console.log(response.profileobj);
-     this.props. history.push('/Home')
-    //  
-     
-   // <Home/>
-    
-    
-    
-  
-   }
-  
-    render() {
-        return (
-            <div>
-                
-                
-           <GoogleLogin
-            clientId="145357618808-oe16u8u6afq634lg3790cv0jkn9066kq.apps.googleusercontent.com"
-              buttonText="Login"
-             onSuccess={this.responseGoogle}
-            onFailure={this.responseGoogle}
-            cookiePolicy={'single_host_origin'}
-           
-          
-    
-            />,
-           
-          
-          <div>
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.responseGoogle}> Checking Rediect </Button>
+        <GoogleLogin
+          clientId="145357618808-oe16u8u6afq634lg3790cv0jkn9066kq.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+        ,
+        <div>
           <GoogleLogout
             clientId="145357618808-oe16u8u6afq634lg3790cv0jkn9066kq.apps.googleusercontent.com"
             buttonText="Logout"
-           onLogoutSuccess={this.logout}
-    >
-     </GoogleLogout>
-   
-
-          </div>
-
-            </div>
-        );
-    }
+            onLogoutSuccess={this.logout}
+          ></GoogleLogout>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-export default withRouter(Google);
+export default Google;
--- src/component/Navber.js
+++ src/component/Navber.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
-import { Route, BrowserRouter as Router, Link, Switch } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navber extends Component {
-    render() {
-        return (
-            <div>
-               <Link to="/home">
-                   home
-               </Link>
-               <Link to="/Login">
-                  login
-               </Link>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Link to="/home">home</Link>
+        <Link to="/Login">login</Link>
+      </div>
+    );
+  }
 }
 
 export default Navber;
